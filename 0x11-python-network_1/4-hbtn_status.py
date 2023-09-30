c02181f24ab733baf9d5810bcb859f77cbc16c06
gamalN <gamaln977@gmail.com>
2023-09-30 00:45:04 +0000

    Initial Commit

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Write a Python script that fetches https://alx-intranet.hbtn.io/status"""
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import requests
+
+
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    response = requests.get(url)
-    print('Body response:')
-    print('\t- type: {}'.format(type(response.text)))
-    print('\t- content: {}'.format(response.text))
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
